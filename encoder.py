413d665cba649b96d3072224bf74feab46850fb8
adamekay <adamkayadamkay@hotmail.com>
2023-07-19 19:04:34 -0400

    fixed encoding function to include case where the number is greater than 6. Also fixed issue where leading 0's weren't processed.

--- encoder.py
+++ encoder.py
@@ -1,8 +1,11 @@
 #Adam Kay
 def encode(password):
     encoded_password = ""
-    for i in str(password):
-        encoded_password += str(int(i) + 3)
+    for i in password:
+        if int(i) > 6:
+            encoded_password += str(int(i) - 7)
+        else:
+            encoded_password += str(int(i) + 3)
     print("Your password has been encoded and stored!\n")
     return int(encoded_password)
 
@@ -27,7 +30,7 @@ if __name__ == "__main__":
               "\n3. Quit")
         selection = int(input("\nPlease enter an option: "))
         if selection == 1:
-            to_be_encoded = int(input("Please enter your password to encode: "))
+            to_be_encoded = input("Please enter your password to encode: ")
             encoded_password = encode(to_be_encoded)
         elif selection == 2:
             print(f"The encoded password is {encoded_password}"
